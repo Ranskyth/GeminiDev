dc2e2dfc5791fde276c14872f18b2c65ed0757a1
Ranskyth <ga.lima2205@gmail.com>
2025-08-25 22:13:48 -0300

    feat:(dashboard) create atributos user

--- frontend/src/app/(pages)/(private)/(client)/missoes/page.tsx
+++ frontend/src/app/(pages)/(private)/(client)/missoes/page.tsx
@@ -4,9 +4,9 @@ export default function Missoes() {
 	return (
 		<div className="bg-[#1B1E26] flex-1 w-full h-screen px-20 py-5">
 			<div>
-				<div className="bg-[#5BBCE5] rounded-3xl flex justify-between px-5">
-					<div className="flex flex-col gap-7">
-						<h1>Olá, gabriel</h1>
+				<div className="bg-[#5BBCE5] rounded-[0.80rem] flex justify-between px-5">
+					<div className="flex flex-col w-69 gap-7">
+						<h1>Olá, <span className="text-2xl font-bold">gabriel</span></h1>
 						<p>
 							Aqui você encontra as missões que precisa realizar. Uma missão
 							pode ser uma tarefa ou um questionário
@@ -18,8 +18,12 @@ export default function Missoes() {
 						alt=""
 					/>
 				</div>
-				<div></div>
-				<div></div>
+				<div>
+
+				</div>
+				<div>
+					
+				</div>
 			</div>
 		</div>
 	);
